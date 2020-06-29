dcd416234d845d496181c4d39e065e77e0c0bc67
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2020-06-29 17:35:01 +0600

    Добавить `install.sh`
    Скпирт устанавливает проектные зависимости.

---
+++ install.sh
@@ -0,0 +1,10 @@
+# Установить зависимости.
+#
+
+# -D значит ставить в "devDependencies" в package.json.
+# Т.о. они не попадут в bundle, как react и react-dom.
+# -g значит ставить глобально.
+npm i -D webpack webpack-cli
+npm i -g webpack webpack-cli
+npm i react react-dom
+npm i -D typescript ts-loader
