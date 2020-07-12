82cfb1b9c8900f2600c7aff93a7724775a8459f9
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2020-07-12 13:18:38 +0600

    Обновить настройки Webpack (2.1)
    Добавить "HTMLWebpackPlugin" и "devServer".

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV == 'development';
+const IS_PROD = NODE_ENV == 'production';
+
+function setupDevtool() {
+  // Настройки для devtool.  У него их
+  // множество.
+  if (IS_DEV) return 'eval';
+  if (IS_PROD) return false;  // Не генерировать source map'ы в продакшне.
+}
 
 // Объект со всеми настройками для Webpack.
 module.exports = {
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
   },
+  // Webpack поддерживает значени 'development',
+  // 'production', none'.
   mode: NODE_ENV ? NODE_ENV : 'development',
   // Откуда начинать. По абсолютному пути. 
   entry: path.resolve(__dirname, 'src/index.jsx'),
@@ -20,5 +32,20 @@ module.exports = {
       test: /\.[tj]sx?$/,
       use: ['ts-loader']
     }]
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({template: path.resolve(__dirname, 'index.html')})
+  ],
+  // Настройки webpack-dev-server.
+  devServer: {
+    port: 3000,
+    // Автоматически перезагружать после
+    // правки исходников.
+    open: true,
+    // Настройка будет работать только в
+    // режиме "development".  И не будет
+    // работать в продакшне.
+    hot: IS_DEV
+  },
+  devtool: setupDevtool()
 };
