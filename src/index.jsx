22a7dce955dee39cc6a6afe63324efdf84ee23fd
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2020-07-12 13:15:30 +0600

    Исправить синтаксис в `src/index.jsx` (2.1)

--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Header } from './Header';
 
-// Такое понимает.
-ReactDOM.render(<Header />, document.getElementById({elementId:'react_root'}));
-// Такого не понимает.
-ReactDOM.render(<Header />, document.getElementById(elementId:'react_root'));
 
-// // // Только после загрузки страницы.
-// window.addEventListener(type: 'load', listener: () => {
-//     // Что куда монтировать.
-// ReactDOM.render(<Header />, document.getElementById(elementId: 'react_root'));
-// });
+// Что делать после загрузки страницы.
+window.addEventListener('load', () => {
+    // Что куда монтировать.
+    ReactDOM.render(<Header />, document.getElementById('react_root'));
+});
