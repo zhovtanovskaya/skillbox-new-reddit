af959b17b05a487ca48b025c3ae37a7805ad2ba8
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2020-07-12 14:21:05 +0600

    Добавить пакет "express" (2.2)
    Это веб-сервер. Запустить его можно командой
    `node src/server/server.js`.

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@
 # -D значит ставить в "devDependencies" в package.json.
 # Т.о. они не попадут в bundle, как react и react-dom.
 # -g значит ставить глобально.
+npm i express
 npm i -D webpack webpack-cli
 npm i -g webpack webpack-cli
 npm i react react-dom
 npm i -D typescript ts-loader
 npm i -D html-webpack-plugin
-npm install -D webpack-dev-server
-npm install -g webpack-dev-server
+npm i -D webpack-dev-server
+npm i -g webpack-dev-server
