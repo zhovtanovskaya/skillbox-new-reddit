ffe65b47101ca6a7f5b883e81234845245ab5d02
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2020-07-18 21:35:48 +0600

    Поправить импорты в файлах из `src/` (2.2)
    С учетом нового дерева папок проекта.

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Header } from './Header';
+import { Header } from '../shared/Header';
 
 
 // Что делать после загрузки страницы.
