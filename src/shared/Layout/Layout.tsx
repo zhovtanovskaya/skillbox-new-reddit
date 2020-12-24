a647b31dcb80e2ffb3bc9a2222e984aa4454800d
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2020-12-24 10:41:56 +0600

    Добавить компонент `Layout` в проект (4.8)
    Добавить папку `src/shared/Layout/` и начать
    в ней `Layout.tsx` и `layout.css`.

---
+++ src/shared/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './layout.css';
+
+interface LayoutProps {
+    children?: React.ReactNode;
+}
+
+export function Layout({ children }: LayoutProps) {
+    return (
+        <div className={styles.layout}>
+            {children}
+        </div>
+    );
+}
