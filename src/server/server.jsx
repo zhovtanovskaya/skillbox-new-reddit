e117c855d80cb6aff08af1c00cff3a71616d5708
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2020-12-26 17:24:54 +0600

    Добавить комментарий в `server.jsx` (2.2)

--- src/server/server.jsx
+++ src/server/server.jsx
@@ -1,3 +1,9 @@
+// Настроить и запустить сервер на порту 3000, который
+// будет отдавать клиенту (браузеру) статические файлы
+// и отрендеренные на сервере компоненты.
+// Сервер автоматически перезапускается при изменении
+// файлов с исходным кодом.
+
 import express from 'express';
 import ReactDOM from 'react-dom/server';
 import * as React from 'react';
@@ -5,10 +11,8 @@ import { Layout } from '../shared/Layout/Layout';
 import { indexTemplate } from './indexTemplate';
 
 const app = express();
-
 // Роуты для статики настроить.
 app.use('/static', express.static('./dist/client'));
-
 app.get('/', (req, res) => {
     res.send(
         indexTemplate(ReactDOM.renderToString(<Layout>Server content.</Layout>)),
