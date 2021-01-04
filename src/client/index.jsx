981b791930d3cc1e1d44820049a82a3875f932e9
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2021-01-04 14:58:59 +0600

    Изменить домашнюю страницу на клиенте (4.5)
    Использовать в `src/client/index.jsx` компонент
    `<App>` вместо `<Layout>`.

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Layout } from '../shared/Layout/Layout';
+import {App} from "../App";
 
 
 // Что делать после загрузки страницы.
 window.addEventListener('load', () => {
     // Что куда монтировать.
-    ReactDOM.hydrate(<Layout>Client content.</Layout>, document.getElementById('react_root'));
+    ReactDOM.hydrate(<App />, document.getElementById('react_root'));
 });
