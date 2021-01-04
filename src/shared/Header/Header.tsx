5d59a16aff378b6158b2530ef7c188e6a3f2838e
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2021-01-04 15:06:12 +0600

    Добавить стили для `<Header>` (4.8)

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import styles from './header.css';
 import {SearchBlock} from './SearchBlock/SearchBlock';
 import {ThreadTitle} from './ThreadTitle/ThreadTitle';
 import {SortBlock} from "./SortBlock/SortBlock";
 
 export function Header() {
     return (
-        <header>
+        <header className={styles.header}>
             <SearchBlock />
             <ThreadTitle />
             <SortBlock />
