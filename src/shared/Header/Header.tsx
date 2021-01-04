1450e6aa9e4d1dfa6138a4202f792ab22e3456da
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2021-01-04 15:02:01 +0600

    Добавить `<SortBlock>` для `<Header>` (4.8)
    Добавить компонент `<SortBlock>` в `src/shared/Header/SortBlock/`
    и использовать его внутри компонента `<Header>`.

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import {SearchBlock} from './SearchBlock/SearchBlock';
 import {ThreadTitle} from './ThreadTitle/ThreadTitle';
+import {SortBlock} from "./SortBlock/SortBlock";
 
 export function Header() {
     return (
         <header>
             <SearchBlock />
             <ThreadTitle />
+            <SortBlock />
         </header>
     )
 }
