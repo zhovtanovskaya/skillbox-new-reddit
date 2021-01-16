478cbb8c7a942c5ad79f5ec82edc7c42d04eb6e0
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2021-01-16 14:26:41 +0600

    Добавить `<GenericList>` (5.3)
    Добавить `src/shared/GenericList/` с `GenericList.tsx`
    и стилями для соответствующего компонента.

---
+++ src/shared/GenericList/GenericList.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+interface Item {
+    id: string;
+    text: string;
+    onClick: (id: string) => void;
+    className: string;
+    As: 'a' | 'li' | 'button' | 'div';
+    href?: string;
+}
+
+interface GenericListProps {
+    list: Item[];
+}
+
+const noop = () => {};
+
+export function GenericList({ list }: GenericListProps) {
+    return (
+        <>
+            {list.map(
+                ({As, text, onClick=noop, className, id, href}) => (
+                    <As
+                        className={className}
+                        onClick={() => onClick(id)}
+                        key={id}
+                        href={href}
+                    >
+                        {text}
+                    </As>
+                )
+            )}
+        </>
+    )
+}
