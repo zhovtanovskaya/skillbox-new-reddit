c09088b0d3f7afadb5e8421c51287e0a1d781d87
Y.Zhovtanovskaya <y.zhovtanovskaya@gmail.com>
2021-03-10 17:28:40 +0600

    Скрывать пункты меню карточки (6.6)
    В мобильной версии карточки (до 1024px) скрывать все
    пункты меню карточки, скроме "Скрыть", "Пожаловаться",
    "Закрыть".

--- src/shared/CardsList/Card/Menu/MenuItemsList/MenuItemsList.tsx
+++ src/shared/CardsList/Card/Menu/MenuItemsList/MenuItemsList.tsx
@@ -8,10 +8,26 @@ interface MenuItemsListProps {
 export function MenuItemsList({ postId }: MenuItemsListProps) {
     return (
         <ul className={styles.menuItemsList}>
+            <div className={styles.desktopMenuItems}>
+                <li className={styles.menuItem}>
+                    Комментарий
+                </li>
+                <div className={styles.divider} />
+                <li className={styles.menuItem}>
+                    Поделиться
+                </li>
+                <div className={styles.divider} />
+            </div>
             <li className={styles.menuItem}>
                 Скрыть
             </li>
-            <div className={styles.divider} />
+            <div className={styles.desktopMenuItems}>
+                <div className={styles.divider} />
+                <li className={styles.menuItem}>
+                    Сохранить
+                </li>
+                <div className={styles.divider} />
+            </div>
             <li className={styles.menuItem}>
                 Пожаловаться
             </li>
